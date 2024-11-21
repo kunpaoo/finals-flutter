75853ac8a0aa13a54ede75c7cb41db27fd5f75ee
anya <txcokun@gmail.com>
2024-11-22 00:18:42 +0800

    feat: added drawers, dashboard and item design

--- lib/screens/item.dart
+++ lib/screens/item.dart
@@ -5,14 +5,13 @@ import 'package:cloud_firestore/cloud_firestore.dart'; // Required for Timestamp
 
 class Item extends StatelessWidget {
   final Map<String, dynamic> item;
-
   const Item({Key? key, required this.item}) : super(key: key);
 
   // Function to parse the date string and convert it into DateTime object
   DateTime parseDateString(String dateString) {
     print(dateString);
-    final datePart = dateString.split(' at ')[0]; 
-    final timePart = dateString.split(' at ')[1].split(' UTC')[0]; 
+    final datePart = dateString.split(' at ')[0];
+    final timePart = dateString.split(' at ')[1].split(' UTC')[0];
 
     // Parse the date and time separately
     DateFormat dateFormat = DateFormat('MMMM dd, yyyy');
@@ -38,76 +37,158 @@ class Item extends StatelessWidget {
   Widget build(BuildContext context) {
     List<String> imageUrls = List<String>.from(item['photoUrls'] ?? []);
 
+    Color primary = const Color.fromARGB(255, 8, 100, 175);
     // Get the date field from Firebase
     var dateField = item['date'];
 
     String formattedDate = '';
     if (dateField != null) {
       if (dateField is Timestamp) {
-        DateTime itemDate = dateField.toDate(); 
+        DateTime itemDate = dateField.toDate();
 
-        formattedDate = DateFormat('MMMM dd, yyyy \n hh:mm:ss a').format(itemDate);
+        formattedDate = DateFormat('MMMM dd, yyyy hh:mm:ss a').format(itemDate);
       } else if (dateField is String) {
         DateTime itemDate = parseDateString(dateField);
-        formattedDate = DateFormat('MMMM dd, yyyy \n hh:mm:ss a').format(itemDate);
+        formattedDate = DateFormat('MMMM dd, yyyy hh:mm:ss a').format(itemDate);
       }
     }
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(item['name'] ?? 'Item Details'),
+        title: const Padding(
+          padding: EdgeInsets.only(left: 8.0),
+          child: Text(
+            'UNIVENTS',
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+        ),
+        leading: Builder(builder: (context) {
+          return Container(
+            margin: const EdgeInsets.only(left: 10),
+            padding: const EdgeInsets.all(5),
+            child: IconButton(
+              icon: Icon(Icons.menu, color: Colors.white),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            ),
+          );
+        }),
+        backgroundColor: const Color.fromARGB(255, 8, 100, 175),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                  color: const Color.fromARGB(255, 8, 100, 175)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Image.network(
+                    "https://www.addu.edu.ph/wp-content/uploads/2020/08/UniversitySealWhite-1024x1020.png",
+                    height: 80,
+                  ),
+                  SizedBox(width: 20),
+                  const Text(
+                    "UNIVENTS",
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      letterSpacing: -1,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              title: const Text('Home'),
+              onTap: () {
+                Navigator.popAndPushNamed(context, '/home');
+              },
+            ),
+            ListTile(
+              title: const Text('Logout'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(color: Colors.white),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             CarouselSlider(
               options: CarouselOptions(
-                height: 300, 
-                enlargeCenterPage: true, 
-                autoPlay: true, 
-                aspectRatio: 16 / 9, 
-                viewportFraction: 0.8, 
+                height: 400,
+                autoPlay: true,
+                aspectRatio: 16 / 9,
+                viewportFraction: 0.8,
               ),
               items: imageUrls.map((url) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.0),
-                        child: Image.network(
-                          url,
-                          width: MediaQuery.of(context).size.width,
-                          height: 250,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    );
-                  },
+                return Image.network(
+                  url,
+                  width: MediaQuery.of(context).size.width,
+                  height: 250,
+                  fit: BoxFit.cover,
                 );
               }).toList(),
             ),
-            const SizedBox(height: 16),
-            Text(
-              'Name: ${item['name'] ?? 'No Name'}',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              'Location: ${item['location'] ?? 'No Location'}',
-              style: TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              'Description: ${item['description'] ?? 'No Description'}',
-              style: TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'Date: $formattedDate',
-              style: TextStyle(fontSize: 16),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 24),
+              width: MediaQuery.sizeOf(context).width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    formattedDate,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    '${item['name'] ?? 'No Name'}',
+                    style: TextStyle(
+                        fontSize: 33,
+                        fontWeight: FontWeight.bold,
+                        color: primary,
+                        height: 1.1),
+                    textAlign: TextAlign.start,
+                  ),
+                  SizedBox(height: 15),
+                  Text(
+                    '${item['description'] ?? 'No Description'}',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.location_on,
+                        color: primary,
+                      ),
+                      SizedBox(width: 10),
+                      Flexible(
+                        child: Text(
+                          '${item['location'] ?? 'No Location'}',
+                          softWrap: true,
+                          overflow: TextOverflow.visible,
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                ],
+              ),
             ),
           ],
         ),
